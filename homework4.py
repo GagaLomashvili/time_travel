0fcce53cc81c65cf45b3a26fc6b18a14344aa74f
GagaLomashvili <138609160+GagaLomashvili@users.noreply.github.com>
2023-07-18 10:22:33 -0600

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,7 @@
+person_age = ("How old are you?")
+
+to_travel = intup ("how many years to travel")
+
+year_after_time_travel = person_age + to_travel 
+
+print ("After Travel, you will be",year_after_time_travel years_old")
